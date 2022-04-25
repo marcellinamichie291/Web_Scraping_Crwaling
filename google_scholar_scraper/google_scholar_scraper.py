ab675aff67de0171734480a8cf570337d34e5b1f
hongcana <xix0717@naver.com>
2022-04-25 22:49:48 +0900

    update version 1.12

--- google_scholar_scraper/google_scholar_scraper.py
+++ google_scholar_scraper/google_scholar_scraper.py
@@ -1,9 +1,16 @@
-#Made by hongcana
-#ver 1.0
+# Made by hongcana
+# ver 1.1 Updates
+
+# fix bugs in get_year, get_author method
+# add time.sleep(1) to avoid response[429]
+# 한글 논문 검색 지원 추가( Support for searching korean papers )
+# 논문 검색 수 입력 가능
+
 import pandas as pd
 import time
 import requests
 from bs4 import BeautifulSoup
+import urllib
 
 # Methods
 def get_citations(content):
@@ -21,6 +28,7 @@ def get_citations(content):
 
 
 def get_year(content):
+    out = ''
     for char in range(0, len(content)):
         if content[char] == '-':
             out = content[char-5:char-1]
@@ -29,6 +37,7 @@ def get_year(content):
     return int(out)
 
 def get_author(content):
+    out = 0
     for char in range(0, len(content)):
         if content[char] == '-':
             out = content[:char-1]
@@ -37,14 +46,15 @@ def get_author(content):
 
 
 # Update these varibales according to your requirement!
+condition = input("한글 논문 검색 = KO / 외국 논문 검색 = EN 입력...")
 keyword = input("키워드를 입력하세요...") # 키워드 입력
 num_of_results = 500
+num_of_results = int(input("추출할 논문 개수 입력..."))
 save_database = True
 # Choose if you would like to save the DB to .csv
 
 # Start new session
 session = requests.Session()
-
 # Variables
 links = list()
 title = list()
@@ -54,32 +64,52 @@ rank = list()
 author = list()
 rank.append(0)
 
+
 # Get content from 1000 URLs
 for n in range(0, num_of_results, 10):
     if n % 100 == 0:
         # Time.sleep 빼셔도 됩니다.
         time.sleep(0.2)
         print("Job processing...{}/{}".format(n, num_of_results))
+    time.sleep(1.01)
+
+    if condition.upper() == 'EN':
+        url = 'https://scholar.google.com/scholar?start='+str(n)+'&q='+keyword.replace(' ','+')
+
+    elif condition.upper() == 'KO':
+        # parse모듈을 사용해 한글("한글")변수를 유니코드로 치환
+        encode = urllib.parse.quote_plus(keyword)
+        url = 'https://scholar.google.com/scholar?start='+str(n)+'&q='+encode.replace(' ','+')+'&hl=ko&lr=lang_ko'
+
+    else:
+        print("오류 : 한글, 영문 논문 여부를 정확히 입력해주세요. (KO 또는 EN 입력!)")
+        condition = -1
+        break
 
     url = 'https://scholar.google.com/scholar?start='+str(n)+'&q='+keyword.replace(' ','+')
     page = session.get(url)
     c = page.content
+    print(page) # check response
 
     # Create parser
     soup = BeautifulSoup(c, 'html.parser')
     # Get stuff
     mydivs = soup.findAll("div", {"class": "gs_r gs_or gs_scl"})
-    
+
+
+    print("Job processing...{}/{}".format(n, num_of_results))
     for div in mydivs:
         try:
             links.append(div.find('h3').find('a').get('href'))
         except: # catch *all* exceptions
             links.append('Look manually at: https://scholar.google.com/scholar?start='+str(n)+'&q=non+intrusive+load+monitoring')
+            links.append('링크를 찾을 수 없는 항목')
 
         try:
             title.append(div.find('h3').find('a').text)
         except:
             title.append('Could not catch title')
+            title.append('제목을 찾을 수 없는 항목')
 
         citations.append(get_citations(div.select('div.gs_ri > div.gs_fl > a:nth-child(3)')))
         year.append(get_year(div.find('div', {'class' : 'gs_a'}).text))
@@ -99,3 +129,17 @@ if save_database:
     timestr = time.strftime("%y%m%d_%H%M%S")
     data_ranked.to_csv(keyword+'_'+timestr+'.csv', encoding='utf-8-sig')
     print('to csv Done!')
+
+if condition != -1:
+    data = pd.DataFrame(zip(author, title, citations, year, links), index = rank[1:],\
+        columns = ['Author', 'Title', 'Citations', 'Year', 'Source'])
+    data.index.name = 'rank'
+
+    data_ranked = data.sort_values('Citations', ascending = False)
+    print(data_ranked)
+    print(data_ranked.shape)
+
+    if save_database:
+        timestr = time.strftime("%y%m%d_%H%M%S")
+        data_ranked.to_csv(keyword+'_'+timestr+'.csv', encoding='utf-8-sig')
+        print('to csv Done!')
